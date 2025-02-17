d3b7008da8946583ca7403b8bd7f95ccaa4522ed
Oliver <57394670+oliverbowp@users.noreply.github.com>
2025-02-17 19:27:40 +0000

    Add fawe paste method

---
+++ src/main/java/com/hastegames/asteroidrandomizer/fawe/AsteroidFawe.java
@@ -0,0 +1,52 @@
+package com.hastegames.asteroidrandomizer.fawe;
+
+import com.fastasyncworldedit.core.FaweAPI;
+import com.hastegames.commons.util.EasyLog;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.math.BlockVector3Imp;
+import com.sk89q.worldedit.world.World;
+import org.bukkit.Location;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
+
+public class AsteroidFawe {
+
+    public CompletableFuture<Void> pasteSchematic(Location location, File schematicFile) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                String fileName = schematicFile.getName();
+
+                if (location.getWorld() == null) {
+                    return;
+                }
+                String worldName = location.getWorld().getName();
+
+                ClipboardFormat clipboardFormat = ClipboardFormats.findByFile(schematicFile);
+                if (clipboardFormat == null) {
+                    EasyLog.toConsole(getClass(), "No clipboard format found for schematic " + fileName);
+                    return;
+                }
+
+                try (InputStream in = Files.newInputStream(schematicFile.toPath())) {
+                    Clipboard clipboard = clipboardFormat.load(in);
+                    if (clipboard == null) {
+                        EasyLog.toConsole(getClass(), "Schematic " + fileName + " has no clipboard");
+                        return;
+                    }
+
+                    World weWorld = FaweAPI.getWorld(worldName);
+                    BlockVector3 vector3 = BlockVector3Imp.at(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+                    clipboard.paste(weWorld, vector3, false, true, null);
+                }
+            } catch (Throwable t) {
+                EasyLog.toConsole(getClass(), "An error occurred while pasting schematic " + schematicFile.getName(), t);
+            }
+        });
+    }
+
+}
